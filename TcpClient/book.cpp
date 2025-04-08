31b33c27ae2217e108e0bad48dec73ff6d9bf3f2
hwlgit config --global user.email 12172214+song-haigang@user.noreply.gitee.com <12172214+song-haigang@user.noreply.gitee.com>
2025-04-08 09:31:13 +0800

    完成文件操作的ui设计

--- TcpClient/book.cpp
+++ TcpClient/book.cpp
@@ -3,5 +3,36 @@
 Book::Book(QWidget *parent)
     : QWidget{parent}
 {
+    m_pBookListW = new QListWidget;
+    m_pReturnPB = new QPushButton("返回");
+    m_pCreateDirPB = new QPushButton("创建文件夹");
+    m_pDelDirPB = new QPushButton("删除文件夹");
+    m_pRenamePB = new QPushButton("重命名文件夹");
+    m_pFlushFilePB = new QPushButton("刷新文件");
+
+    QVBoxLayout *pDirVBL = new QVBoxLayout;   //垂直布局
+    pDirVBL->addWidget(m_pReturnPB);
+    pDirVBL->addWidget(m_pCreateDirPB);
+    pDirVBL->addWidget(m_pDelDirPB);
+    pDirVBL->addWidget(m_pRenamePB);
+    pDirVBL->addWidget(m_pFlushFilePB);
+
+    m_pUploadPB = new QPushButton("上传文件");
+    m_pDownLoadPB = new QPushButton("下载文件");
+    m_pDelFilePB = new QPushButton("删除文件");
+    m_pShareFilePB = new QPushButton("共享文件");
+
+    QVBoxLayout *pFileVBL = new QVBoxLayout;
+    pFileVBL->addWidget(m_pUploadPB);
+    pFileVBL->addWidget(m_pDownLoadPB);
+    pFileVBL->addWidget(m_pDelFilePB);
+    pFileVBL->addWidget(m_pShareFilePB);
+
+    QHBoxLayout *pMain = new QHBoxLayout;
+    pMain->addWidget(m_pBookListW);
+    pMain->addLayout(pDirVBL);
+    pMain->addLayout(pFileVBL);
+
+    setLayout(pMain);
 
 }
--- TcpClient/book.h
+++ TcpClient/book.h
@@ -2,6 +2,10 @@
 #define BOOK_H
 
 #include <QWidget>
+#include<QListWidget>
+#include<QPushButton>
+#include<QHBoxLayout>
+#include<QVBoxLayout>
 
 class Book : public QWidget
 {
@@ -10,6 +14,18 @@ public:
     explicit Book(QWidget *parent = nullptr);
 
 signals:
+
+private:
+    QListWidget *m_pBookListW;       //显示文件列表
+    QPushButton *m_pReturnPB;        //返回
+    QPushButton *m_pCreateDirPB;     //创建文件夹
+    QPushButton *m_pDelDirPB;        //删除文件夹
+    QPushButton *m_pRenamePB;        //重命名文件夹
+    QPushButton *m_pFlushFilePB;     //刷新文件夹
+    QPushButton *m_pUploadPB;        //上传文件
+    QPushButton *m_pDownLoadPB;      //下载文件
+    QPushButton *m_pDelFilePB;       //删除文件
+    QPushButton *m_pShareFilePB;     //分享文件
 };
 
 #endif // BOOK_H
